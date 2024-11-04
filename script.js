51fc3dd0032b5edaf271d0dd0fa80ab4b3f76ead
lucas mendes <lucasmndes.dev@gmail.com>
2024-11-04 15:10:58 -0300

    efeito modal

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.querySelector("span").onclick = () => {
     document.body.classList.toggle("dark-mode")
 }
+
+
+const galeriaModal = document.querySelector(".galeria-modal")
+
+const imagem = document.querySelector(".galeria-modal img")
+
+function fecharGaleria() {
+    galeriaModal.style.display = "none"
+    imagem.style.transform = "scale(0)"
+}
+
+function abrirGaleria(src) {
+    galeriaModal.style.display = "block"
+    imagem.style.transform = "scale(1)"
+    imagem.src = src
+}
